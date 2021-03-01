93eabc4584b770e5cffc7a469bf393f448d2491e
Lyl <2470642801@qq.com>
2021-03-01 21:59:14 +0800

    1、把原本的获取信息类去掉。 2.把多余的端口、删除。

--- src/ui/MenuWindow.java
+++ src/ui/MenuWindow.java
@@ -49,11 +49,8 @@ public class MenuWindow {
                     }
             );
             cb = new JComboBox();
-            cb.addItem("All");
-            JButton jb = new JButton("传文件");
             JPanel pl = new JPanel(new BorderLayout());
             pl.add(cb);
-            pl.add(jb, BorderLayout.WEST);
             JPanel p = new JPanel(new BorderLayout());
             p.add(pl, BorderLayout.WEST);
             p.add(tf);
@@ -61,118 +58,6 @@ public class MenuWindow {
             f.getContentPane().add(sp);
             f.setVisible(true);
 
-            //开启收消息线程
-            GetMessage getMessage = new GetMessage(this);
-            getMessage.start();
-
-        /*
-        提示XXX进入聊天室
-         */
-            showXXXIntoChatRoom();
-
-        /*
-        提示XXX正在聊天室
-         */
-            showXXXInChatRoom();
-        }
-        public void sendAll() {
-            String uri = "jdbc:sqlserver://localhost:1433;integratedSecurity=true;DatabaseName=library";
-            String user = "sa";
-            String Password = "123456";
-            PreparedStatement pstmt = null;
-            Connection conn = null;
-            try {
-                conn = DriverManager.getConnection(uri,user, Password);
-                String sql = "SELECT username,ip,port FROM users WHERE status='online'";
-                pstmt = conn.prepareStatement(sql);
-                ResultSet rs = pstmt.executeQuery();
-                while (rs.next()) {
-                    String username = rs.getString("USERNAME");
-                    String ip = rs.getString("IP");
-                    int port = rs.getInt("PORT");
-                    System.out.println(ip);
-                    System.out.println(port);
-                    byte[] ipB = new byte[4];
-
-                    String ips[] = ip.split("\\.");
-                    for (int i = 0; i < ips.length; i++) {
-                        ipB[i] = (byte) Integer.parseInt(ips[i]);
-                    }
-                    if (!username.equals(name)) {
-                        String message = tf.getText();
-                        byte[] m = message.getBytes();
-                        DatagramPacket dp = new DatagramPacket(m, m.length);
-                        dp.setAddress(InetAddress.getByAddress(ipB));
-                        dp.setPort(port);
-                        DatagramSocket ds = new DatagramSocket();
-                        ds.send(dp);//投递
-                    }
-                }
-            } catch (SQLException | UnknownHostException | SocketException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        public void showXXXInChatRoom() {
-            String uri = "jdbc:sqlserver://localhost:1433;integratedSecurity=true;DatabaseName=library";
-            String user = "sa";
-            String password = "123456";
-            PreparedStatement pstmt = null;
-            Connection conn = null;
-            try {
-                conn = DriverManager.getConnection(uri, user, password);
-                String sql = "SELECT username,ip,port FROM users WHERE status='online' AND username!=?";
-                pstmt = conn.prepareStatement(sql);
-                pstmt.setString(1,name);
-                ResultSet rs = pstmt.executeQuery();
-                while (rs.next()) {
-                    String username = rs.getString("USERNAME");
-                    String message = username + "正在聊天室";
-                    ta.append(message);
-                    cb.addItem(username);
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        public void showXXXIntoChatRoom() {
-            String uri = "jdbc:sqlserver://localhost:1433;integratedSecurity=true;DatabaseName=library";
-            String user = "sa";
-            String password = "123456";
-            PreparedStatement pstmt = null;
-            Connection conn = null;
-            try {
-                conn = DriverManager.getConnection(uri, user, password);
-                String sql = "SELECT username,ip,port FROM users WHERE status='online'";
-                pstmt = conn.prepareStatement(sql);
-                ResultSet rs = pstmt.executeQuery();
-                while (rs.next()) {
-                    String username = rs.getString("USERNAME");
-                    String ip = rs.getString("IP");
-                    int port = rs.getInt("PORT");
-                    System.out.println(ip);
-                    System.out.println(port);
-                    byte[] ipB = new byte[4];
-
-                    String ips[] = ip.split("\\.");
-                    for (int i = 0; i < ips.length; i++) {
-                        ipB[i] = (byte) Integer.parseInt(ips[i]);
-                    }
-                    if (!username.equals(name)) {
-                        String message = name + "进入了聊天室";
-                        byte[] m = message.getBytes();
-                        DatagramPacket dp = new DatagramPacket(m, m.length);
-                        dp.setAddress(InetAddress.getByAddress(ipB));
-                        dp.setPort(port);
-                        DatagramSocket ds = new DatagramSocket();
-                        ds.send(dp);//投递
-                    }
-                }
-            } catch (SQLException | UnknownHostException | SocketException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
 }
+
